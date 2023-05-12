f7e1ad36d2dcfae55baee74c2d5392e4d64997af
ArtemStrl <berdygin_2009@list.ru>
2023-05-12 11:34:07 +0900

    the initial edition

---
+++ assets/js/text.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["Что-то между мидлом и джуном", "Вжух!", "GITHUB PAGE :)", "Python Dev", "coder", "vzlom jopi", "hmmm...", "bruh", "Hack system...", "h@Cк3D!"],
+        typeSpeed: 30,
+        backSpeed: 0,
+        cursorChar: ['❚'],
+        smartBackspace: true,
+        fadeOut: true,
+        loop: true,
+    });
+});
